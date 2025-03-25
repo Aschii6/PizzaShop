c61b9802d6d38fa529cca753f775e94f7085c25b
Lechea Daniel-Ioan <danutlechea2003@yahoo.com>
2025-03-25 18:12:53 +0200

    L2 Initial setup + initial work

---
+++ src/test/java/pizzashop/service/PizzaServiceTest.java
@@ -0,0 +1,37 @@
+package pizzashop.service;
+
+import org.junit.jupiter.api.*;
+
+class PizzaServiceTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("Test for adding a valid payment with ECP")
+    void addPaymentValidECP() {
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Test for adding a non-valid payment with ECP")
+    void addPaymentNonValidECP() {
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Test for adding a valid payment with BVA")
+    void addPaymentValidBVA() {
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Test for adding a non-valid payment with BVA")
+    void addPaymentNonValidBVA() {
+    }
+}
